1f10158ab6316cb603a306f100744db17249afb0
Juan <jfhg79@gmail.com>
2011-12-14 14:58:54 -0530

    actualizando

--- src/dao/competencia/DaoCompetencia.java
+++ src/dao/competencia/DaoCompetencia.java
@@ -1,8 +1,21 @@
+
 package dao.competencia;
 
-import dao.general.GenericDAO;
+import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
 
-public class DaoCompetencia extends GenericDAO {
+import dao.prueba.GenericDao;
+
+public class DaoCompetencia extends GenericDao {
+	
+	public List listarActivos(Class o){
+		Session session = getSession();
+		Transaction tx =  session.beginTransaction();
+		List lista = session.createCriteria(o).add(Restrictions.eq("estadoCompetencia.codigoEstadoCompetencia", "1")).list();
+		return lista;
+	}
 
 }
