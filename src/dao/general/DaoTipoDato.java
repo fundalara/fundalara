fe16d94f81dc0fc72e7dae8897d95eedf7c5d72c
leyner castillo <leyner_castle@hotmail.com>
2011-12-29 11:18:52 -0430

    Descripcion: Adicion de dos lineas al metodo buscarPorCodigo que no estaba bien implementado Nombre: Leyner Equipo: Entrenamiento

--- src/dao/general/DaoTipoDato.java
+++ src/dao/general/DaoTipoDato.java
@@ -1,9 +1,10 @@
 package dao.general;
 
-import modelo.ActividadEntrenamiento;
 import modelo.TipoDato;
 
 import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Restrictions;
 
 import dao.generico.GenericDao;
@@ -12,6 +13,8 @@ public class DaoTipoDato extends GenericDao {
 	
 	public TipoDato buscarPorTipo(String tipo) {
 		// TODO Auto-generated method stub
+		Session session = getSession();
+		Transaction tx =  session.beginTransaction();
 		Criteria c = getSession().createCriteria(TipoDato.class);
 		c.add(Restrictions.eq("nombre", tipo));
 		return (TipoDato) c.list().get(0);
