2a467942b14fe4f4b68d07d031216adca1173b17
leyner <leyner@leyner-P25G>
2012-01-04 12:12:42 -0430

    DESCRIPCION: SE MODIFICO EL BUSCAR POR TIPO DE DATO BASICO NOMBRE: NOHELY PERNALETE EQUIPO: COMPETENCIA

--- src/dao/general/DaoDatoBasico.java
+++ src/dao/general/DaoDatoBasico.java
@@ -15,29 +15,31 @@ public class DaoDatoBasico extends GenericDao {
 
 	public List<DatoBasico> buscarPorTipoDato(TipoDato td) {
 		// TODO Auto-generated method stub
+		Session session = getSession();
+		Transaction tx = session.beginTransaction();
 		Criteria c = getSession().createCriteria(DatoBasico.class);
 		c.add(Restrictions.eq("tipoDato", td));
 		c.add(Restrictions.eq("estatus", "A"));
 		return c.list();
 	}
-	
+
 	public DatoBasico buscarPorCodigo(String td) {
 		// TODO Auto-generated method stub
 		Criteria c = getSession().createCriteria(DatoBasico.class);
 		c.add(Restrictions.eq("codigoDatoBasico", td));
 		return (DatoBasico) c.list().get(0);
 	}
-	
 
-	public List<DatoBasico> listarParroquias(){
-		  
-		  Session session = getSession();
-		  Transaction tx = session.beginTransaction();
-		  Criteria c = session.createCriteria(TipoDato.class);
-		  TipoDato td =  (TipoDato) c.add(Restrictions.eq("nombre", "parroquia")).list().get(0);
-          c = session.createCriteria(DatoBasico.class);
-          List list = c.add(Restrictions.eq("tipoDato",td)).list();
-          return list;
-		  
-	  }
+	public List<DatoBasico> listarParroquias() {
+
+		Session session = getSession();
+		Transaction tx = session.beginTransaction();
+		Criteria c = session.createCriteria(TipoDato.class);
+		TipoDato td = (TipoDato) c.add(Restrictions.eq("nombre", "parroquia"))
+				.list().get(0);
+		c = session.createCriteria(DatoBasico.class);
+		List list = c.add(Restrictions.eq("tipoDato", td)).list();
+		return list;
+
+	}
 }
