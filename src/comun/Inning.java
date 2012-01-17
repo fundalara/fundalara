740b2ae8dfa92170e74f25e70d53c91a1dd91ec7
unknown <kng3dX@sembonzakura-PC.(none)>
2012-01-17 03:38:24 -0430

    cambios en frmRegistroResultados

---
+++ src/comun/Inning.java
@@ -0,0 +1,28 @@
+package comun;
+
+public class Inning {
+	
+	private int valor;
+	private int indice;
+	
+	public Inning(int valor, int indice) {
+		this.valor = valor;
+		this.indice = indice;
+	}
+	public int getValor() {
+		return valor;
+	}
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	public int getIndice() {
+		return indice;
+	}
+	public void setIndice(int indice) {
+		this.indice = indice;
+	}
+	
+	
+	
+
+}
--- src/controlador/competencia/CntrlFrmRegistroResultados.java
+++ src/controlador/competencia/CntrlFrmRegistroResultados.java
@@ -1,6 +1,5 @@
 package controlador.competencia;
 
-
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -13,11 +12,14 @@ import modelo.Juego;
 import modelo.PersonalForaneo;
 import modelo.PersonalForaneoJuego;
 
+import org.python.modules.synchronize;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.event.Event;
 import org.zkoss.zk.ui.event.InputEvent;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
+import org.zkoss.zul.Column;
+import org.zkoss.zul.Columns;
 import org.zkoss.zul.Combobox;
 import org.zkoss.zul.Grid;
 import org.zkoss.zul.Label;
@@ -26,8 +28,11 @@ import org.zkoss.zul.Listcell;
 import org.zkoss.zul.Listhead;
 import org.zkoss.zul.Listheader;
 import org.zkoss.zul.Listitem;
+import org.zkoss.zul.Row;
 import org.zkoss.zul.Spinner;
 import org.zkoss.zul.Textbox;
+
+import comun.Inning;
 
 import servicio.implementacion.ServicioDatoBasico;
 import servicio.implementacion.ServicioPersonalForaneo;
@@ -42,7 +47,7 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 	Equipo equipoA;
 	Equipo equipoB;
 	List<EquipoJuego> equipos;
-	List<Integer> innings;
+	List<Inning> innings;
 	Listbox lsbxEquipos;
 	Listbox lsbxInnigs;
 	Listhead lhLsbxInnigs;
@@ -60,7 +65,9 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 	Combobox cmbUmpires;
 	Combobox cmbPosiciones;
 	List<PersonalForaneoJuego> umpiresJuego;
-	
+	Grid resultados;
+	Boolean sw = true;
+
 	public List<PersonalForaneo> getUmpires() {
 		return umpires;
 	}
@@ -84,13 +91,13 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 	public void setUmpiresJuego(List<PersonalForaneoJuego> umpiresJuego) {
 		this.umpiresJuego = umpiresJuego;
 	}
-	
-	public void restaurar(){ 
-		//cmbUmpires.setText("-- Seleccione --");
+
+	public void restaurar() {
+		// cmbUmpires.setText("-- Seleccione --");
 		umpires = servicioPersonalForaneo.listarUmpires();
 		posiciones = servicioDatoBasico.listarPosiciones();
 	}
-	
+
 	@Override
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
@@ -98,18 +105,23 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 		formulario = comp;
 		umpiresJuego = new ArrayList<PersonalForaneoJuego>();
 		restaurar();
-	
+
 	}
 
 	public void onCreate$FrmRegistroResultados() {
 		juego = (Juego) formulario.getVariable("juego", false);
+		System.out.println(juego.getCodigoJuego());
 		equipos = ConvertirConjuntoALista(juego.getEquipoJuegos());
 		equipoA = equipos.get(0).getEquipoCompetencia().getEquipo();
 		equipoB = equipos.get(1).getEquipoCompetencia().getEquipo();
 		txtJuego.setText(equipoA.getNombre() + " vs " + equipoB.getNombre());
-		lblEquipoA.setValue(equipoA.getNombre());
-		lblEquipoB.setValue(equipoB.getNombre());
-		
+		// lblEquipoA.setValue(equipoA.getNombre());
+
+		// lblEquipoB.setValue(equipoB.getNombre());
+		innings = new ArrayList<Inning>();
+		innings.add(new Inning(1, 0));
+		innings.add(new Inning(2, 0));
+		innings.add(new Inning(3, 0));
 		binder.loadAll();
 	}
 
@@ -119,37 +131,53 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 			l.add(i.next());
 		}
 		return l;
-		
-		
+
+	}
+
+	public void onChanging$spnrInnigs(InputEvent e) {
+
+		Columns cols = (Columns) formulario.getFellow("titulo");
+		Column col = new Column(e.getValue());
+		if (!buscar(cols, e.getValue())) {
+				col.setWidth("55px");
+				Column columnCarreras = (Column) formulario.getFellow("columnCarreras");
+				cols.insertBefore(col, columnCarreras);
+				Row fila1 = (Row) formulario.getFellow("fila1");
+				Row fila2 = (Row) formulario.getFellow("fila2");
+				Spinner spr1 = new Spinner(0);
+				spr1.setCols(1);
+				spr1.setId("spnrA"+e.getValue());
+				Spinner spr2 = new Spinner(0);
+				spr2.setCols(1);
+				spr2.setId("spnrB"+e.getValue());
+				Label lblCarrerasA = (Label) formulario.getFellow("lblCarrerasA");
+				Label lblCarrerasB = (Label) formulario.getFellow("lblCarrerasB");
+				fila1.insertBefore(spr1, lblCarrerasA);
+				fila2.insertBefore(spr2, lblCarrerasB);
+				resultados.invalidate();
+				e.stopPropagation();
+	     }
 		
 	}
 
-//	public void onChanging$spnrInnigs(InputEvent e){
-//	     
-//		 Listheader lhr = new Listheader(e.getValue());
-//		
-//		 lhLsbxInnigs.appendChild(lhr);
-//		 List<Listitem> items = lsbxInnigs.getItems();
-//		 for (Iterator i = items.iterator(); i.hasNext();) {
-//           //  Listitem  = (Listitem) i.next();
-//             Listcell lc = new Listcell();
-//             Spinner spr = new Spinner(0);
-//        
-//             lc.appendChild(spr);
-//             liEquipoA.appendChild(lc);
-//             liEquipoB.appendChild(lc);
-//            
-//             
-//		}
-//	     binder.loadAll();
-//	     
-//	}
-	
-	public void onClick$btnAgregar(){
-		PersonalForaneoJuego pfj= new PersonalForaneoJuego();
+	public Boolean buscar(Columns cols, String valor) {
+		List lista = cols.getChildren();
+		for (int i = 0; i < lista.size(); i++) {
+			Column col = (Column) lista.get(i);
+			if (col.getLabel().equals(valor)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void onClick$btnAgregar() {
+		PersonalForaneoJuego pfj = new PersonalForaneoJuego();
 		pfj.setJuego(juego);
-		pfj.setPersonalForaneo((PersonalForaneo) cmbUmpires.getSelectedItem().getValue());
-		pfj.setDatoBasico((DatoBasico) cmbPosiciones.getSelectedItem().getValue());
+		pfj.setPersonalForaneo((PersonalForaneo) cmbUmpires.getSelectedItem()
+				.getValue());
+		pfj.setDatoBasico((DatoBasico) cmbPosiciones.getSelectedItem()
+				.getValue());
 		umpiresJuego.add(pfj);
 		binder.loadAll();
 	}
@@ -186,14 +214,12 @@ public class CntrlFrmRegistroResultados extends GenericForwardComposer {
 		this.equipos = equipos;
 	}
 
-	public List<Integer> getInnings() {
+	public List<Inning> getInnings() {
 		return innings;
 	}
 
-	public void setInnings(List<Integer> innings) {
+	public void setInnings(List<Inning> innings) {
 		this.innings = innings;
 	}
-
-	
 
 }
