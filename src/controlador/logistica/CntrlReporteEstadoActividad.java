38aaf626bd879e2cba2f22303aa76b4288549f6f
Reinaldo López <lopezreinaldoernesto@gmail.com>
2012-01-31 02:39:19 -0500

    Agregando archivos de reporte

---
+++ src/controlador/logistica/CntrlReporteEstadoActividad.java
@@ -0,0 +1,78 @@
+package controlador.logistica;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporter;
+import net.sf.jasperreports.engine.JRExporterParameter;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.view.JasperViewer;
+
+import org.zkoss.util.media.AMedia;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Sessions;
+import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.Datebox;
+import org.zkoss.zul.Iframe;
+
+import comun.ConeccionBD;
+import comun.TipoDatoBasico;
+
+public class CntrlReporteEstadoActividad extends GenericForwardComposer{
+	
+	Datebox dboxFechaInicio;
+	Datebox dboxFechaFin;
+	private Connection con;
+	private String jrxmlSrc;
+	Map parameters = new HashMap();
+	Iframe ifReport;
+	
+	
+	public void doAfterCompose(Component comp)throws Exception{
+		super.doAfterCompose(comp);		
+		comp.setVariable("cntrl", this, false);
+		
+		}
+	
+	
+	
+	public void showReportfromJrxml() throws JRException, IOException{
+		JasperReport jasp = JasperCompileManager.compileReport(jrxmlSrc);
+		JasperPrint jaspPrint = JasperFillManager.fillReport(jasp, parameters, con);
+		ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+		JRExporter exporter = new JRPdfExporter();
+		exporter.setParameters(parameters);
+		exporter.setParameter(JRExporterParameter.JASPER_PRINT ,jaspPrint);
+		exporter.setParameter(JRExporterParameter.OUTPUT_STREAM,arrayOutputStream);
+		exporter.exportReport();
+		arrayOutputStream.close();
+		final AMedia amedia = new AMedia("ReporteEstadistico.pdf","pdf","pdf/application", arrayOutputStream.toByteArray());
+		ifReport.setContent(amedia);
+	}
+	// ---------------------------------------------------------------------------------------------------
+	public void onClick$btnImprimir() throws SQLException, JRException, IOException {
+		con = ConeccionBD.getCon("postgres","postgres","123456");
+		//jrxmlSrc = Sessions.getCurrent().getWebApp().getRealPath("/Logistica/Reportes/ReporteEstadistico.jrxml");
+		jrxmlSrc = "C:\\Users\\Reinaldo López\\Documents\\repositorio\\fundalara\\WebContent\\Logistica\\Reportes\\ReporteEstadistico.jrxml";
+		
+		
+		parameters.put("FechaInicio", dboxFechaInicio.getValue());
+	    parameters.put("FechaFin", dboxFechaFin.getValue());
+		
+		showReportfromJrxml();
+	}
+	
+	
+}
