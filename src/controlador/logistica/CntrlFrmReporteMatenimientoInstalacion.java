369e8ff5a32678deb33bf0c7314ffcc0219bb4c2
Reinaldo López <lopezreinaldoernesto@gmail.com>
2012-02-15 05:24:35 -0500

    Agregado controlador

--- src/controlador/logistica/CntrlFrmReporteMatenimientoInstalacion.java
+++ src/controlador/logistica/CntrlFrmReporteMatenimientoInstalacion.java
@@ -1,73 +1,33 @@
 package controlador.logistica;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 
 import modelo.Actividad;
 import modelo.DatoBasico;
 import modelo.EstadoActividad;
 import modelo.Instalacion;
-import modelo.Material;
 import modelo.MaterialActividad;
 import modelo.TareaActividad;
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JRExporter;
-import net.sf.jasperreports.engine.JRExporterParameter;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRTextExporter;
-import net.sf.jasperreports.engine.export.JRTextExporterParameter;
-import net.sf.jasperreports.view.JasperViewer;
 
-import org.hibernate.mapping.Array;
-import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.Executions;
-import org.zkoss.zk.ui.Sessions;
-import org.zkoss.zk.ui.event.Event;
-import org.zkoss.zk.ui.event.EventListener;
-import org.zkoss.zk.ui.event.Events;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
 import org.zkoss.zkplus.databind.AnnotateDataBinder;
-import org.zkoss.zkplus.databind.BindingListModelList;
-import org.zkoss.zul.Button;
-import org.zkoss.zul.Checkbox;
 import org.zkoss.zul.Combobox;
-import org.zkoss.zul.Comboitem;
 import org.zkoss.zul.Datebox;
-import org.zkoss.zul.Label;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Messagebox;
-import org.zkoss.zul.Panel;
-import org.zkoss.zul.Spinner;
-import org.zkoss.zul.Textbox;
-import org.zkoss.zul.Timebox;
+import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.Window;
 
-import servicio.implementacion.ServicioActividad;
 import servicio.implementacion.ServicioDatoBasico;
 import servicio.implementacion.ServicioEstadoActividad;
-import servicio.implementacion.ServicioInstalacion;
-import servicio.implementacion.ServicioMaterial;
 import servicio.implementacion.ServicioMaterialActividad;
 import servicio.implementacion.ServicioTareaActividad;
 import servicio.interfaz.IServicioActividad;
-import servicio.interfaz.IServicioDatoBasico;
-
-
-import comun.TipoDatoBasico;
 
 public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardComposer {
-
-
 
 	private AnnotateDataBinder binder;
 	private IServicioActividad servicioActividad;
@@ -76,7 +36,6 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 	private ServicioTareaActividad servicioTareaActividad;
 	private ServicioMaterialActividad servicioMaterialActividad;
 
-
 	private List<Actividad> listaMantenimiento = new ArrayList<Actividad>();
 	private List<DatoBasico> listaDatoBasico = new ArrayList<DatoBasico>();
 	private List<DatoBasico> listaDatoBasico2 = new ArrayList<DatoBasico>();
@@ -84,7 +43,7 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 	private List<Instalacion> listaInstalacion = new ArrayList<Instalacion>();
 	private List<TareaActividad> listaTareaActividad = new ArrayList<TareaActividad>();
 	private List<MaterialActividad> listaMaterialActividad = new ArrayList<MaterialActividad>();
-			
+
 	private Actividad mantenimiento;
 	private DatoBasico datoBasico;
 	private DatoBasico datoBasico2;
@@ -92,47 +51,43 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 	private TareaActividad tareaActividad;
 	private MaterialActividad materialActividad;
 	private String string;
-	private int a=0, b=0, c=0;
-	
-	
+	private int a = 0, b = 0, c = 0;
+
 	private Window frmReporte;
 	private Combobox cmbMantenimiento;
 	private Datebox fechaInicio;
 	private Datebox fechaFin;
-	
 
 	public void doAfterCompose(Component comp) throws Exception {
 		super.doAfterCompose(comp);
 		comp.setVariable("cntrl", this, false);
-		frmReporte = (Window) comp;	
-		
+		frmReporte = (Window) comp;
+
 		for (int i = 251; i < 255; i++) {
 			listaDatoBasico.add(servicioDatoBasico.buscarPorCodigo(i));
 		}
-		
+
 		for (int i = 220; i < 223; i++) {
 			listaDatoBasico2.add(servicioDatoBasico.buscarPorCodigo(i));
 		}
-		
-		
+
 	}
 
-	public void onChange$fechaFin(){
-		
+	public void onChange$fechaFin() {
 
-		//System.out.println(g);
-		
+		// System.out.println(g);
+
 		binder.loadAll();
 	}
-	
-	public void onSelect$cmbEstadoMantenimiento(){
+
+	public void onSelect$cmbEstadoMantenimiento() {
 		listaMantenimiento.clear();
 		listaTareaActividad.clear();
 		listaMaterialActividad.clear();
 		binder.loadAll();
 	}
-	
-	public void onSelect$cmbInstalacion(){
+
+	public void onSelect$cmbInstalacion() {
 		fechaInicio.getValue();
 		fechaFin.getValue();
 		listaMantenimiento.clear();
@@ -142,48 +97,115 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 		List<Actividad> lista2 = new ArrayList<Actividad>();
 		lista2 = servicioActividad.listarMantenimientos(fechaInicio.getValue(), fechaFin.getValue());
 		listaEstadoActividad = servicioEstadoActividad.listar();
-		
-		
+
 		for (int i = 0; i < listaEstadoActividad.size(); i++) {
-			if (listaEstadoActividad.get(i).getDatoBasico().getCodigoDatoBasico()==datoBasico.getCodigoDatoBasico()){
+			if (listaEstadoActividad.get(i).getDatoBasico().getCodigoDatoBasico() == datoBasico.getCodigoDatoBasico()) {
 				for (int j = 0; j < lista2.size(); j++) {
-					if(lista2.get(j).getCodigoActividad()==listaEstadoActividad.get(i).getActividad().getCodigoActividad()){
-							if(listaEstadoActividad.get(i).getActividad().getInstalacionUtilizada().getInstalacion().getDatoBasico().getCodigoDatoBasico()==datoBasico2.getCodigoDatoBasico()){
-								listaMantenimiento.add(listaEstadoActividad.get(i).getActividad());
-							}
+					if (lista2.get(j).getCodigoActividad() == listaEstadoActividad.get(i).getActividad().getCodigoActividad()) {
+						if (listaEstadoActividad.get(i).getActividad().getInstalacionUtilizada().getInstalacion().getDatoBasico()
+								.getCodigoDatoBasico() == datoBasico2.getCodigoDatoBasico()) {
+							listaMantenimiento.add(listaEstadoActividad.get(i).getActividad());
 						}
-						
 					}
+
 				}
 			}
-	a=listaMantenimiento.size();	
-	binder.loadAll();
+		}
+		a = listaMantenimiento.size();
+		binder.loadAll();
 	}
-	
-	public void onSelect$lboxInstalacion(){
+
+	public void onSelect$lboxInstalacion() {
 		listaTareaActividad.clear();
 		listaMaterialActividad.clear();
 		List<TareaActividad> lista = new ArrayList<TareaActividad>();
 		string = "";
-		
+
 		lista = servicioTareaActividad.listar();
 		listaMaterialActividad = servicioMaterialActividad.ListarPorActividad(mantenimiento);
-		
+
 		for (int i = 0; i < lista.size(); i++) {
-			if (lista.get(i).getActividad()==mantenimiento){
+			if (lista.get(i).getActividad() == mantenimiento) {
 				listaTareaActividad.add(lista.get(i));
 			}
 		}
-		b=listaTareaActividad.size();
-		c=listaMaterialActividad.size();
+		b = listaTareaActividad.size();
+		c = listaMaterialActividad.size();
 		binder.loadAll();
 
-
 	}
-		
-		
-	
-	public void onClick$btnExportar() throws JRException{
+
+	public void onClick$btnExportar() {
+
+		File archivo = null;
+		PrintWriter impresora = null;
+
+		// String rutaExportar =
+		// Sessions.getCurrent().getWebApp().getRealPath("WEB-INF/reportes/ReporteMantenimiento.txt");
+
+		try {
+			archivo = new File("ReporteMantenimientoInstalacion.txt");
+			impresora = new PrintWriter(archivo);
+
+			String idato = "Tipo de instalacion: " + this.datoBasico2.getDescripcion();
+			String idato2 = "Estado de la actividad: " + this.datoBasico.getNombre();
+			String fechainicio = "Fecha de inicio: " + this.fechaInicio.getValue().toString();
+			String fechafin = "Fecha de fin: " + this.fechaFin.getValue().toString();
+			String Man = "Actividad: " + this.mantenimiento.getPlanificacionActividad().getDescripcion();
+
+			impresora.println(idato);
+			impresora.println(idato2);
+			impresora.println(fechainicio);
+			impresora.println(fechafin);
+			impresora.println(Man);
+
+			String ta = "Lista de Materiales";
+			impresora.println(ta);
+			for (int i = 0; i < this.listaMaterialActividad.size(); i++) {
+				impresora.println(this.listaMaterialActividad.get(i).getMaterial().getDescripcion() + " "
+						+ this.listaMaterialActividad.get(i).getCantidadEntregada());
+			}
+			String tm = "Total Materiales: " + this.listaMaterialActividad.size();
+			impresora.println(tm);
+
+			String ts = "Lista de Materiales";
+			impresora.println(ts);
+			for (int i = 0; i < this.listaTareaActividad.size(); i++) {
+				impresora.println(this.listaTareaActividad.get(i).getDatoBasicoByCodigoTarea().getNombre() + " "
+						+ this.listaTareaActividad.get(i).getDatoBasicoByEstadoTarea().getNombre());
+			}
+			String tt = "Total Tareas: " + this.listaTareaActividad.size();
+			impresora.println(tt);
+
+			impresora.close();
+
+			FileInputStream fis = new FileInputStream(archivo);
+			System.out.println("este es fis " + fis);
+
+			byte fileContent[] = new byte[(int) archivo.length()];
+
+			System.out.println("este es filecontent " + fileContent);
+			fis.read(fileContent);
+
+			String strFileContent = new String(fileContent);
+			System.out.println("este es strfilecontent " + strFileContent);
+
+			Filedownload.save(strFileContent.getBytes(), "text/plain", "ReporteMantenimientoInstalacion.txt");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+
+				if (null != archivo) {
+					// archivo.close();
+
+				}
+
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
 
 	}
 
@@ -298,7 +320,7 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 	public void setB(int b) {
 		this.b = b;
 	}
-	
+
 	public int getC() {
 		return c;
 	}
@@ -307,13 +329,11 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 		this.c = c;
 	}
 
-
 	public List<MaterialActividad> getListaMaterialActividad() {
 		return listaMaterialActividad;
 	}
 
-	public void setListaMaterialActividad(
-			List<MaterialActividad> listaMaterialActividad) {
+	public void setListaMaterialActividad(List<MaterialActividad> listaMaterialActividad) {
 		this.listaMaterialActividad = listaMaterialActividad;
 	}
 
@@ -324,8 +344,5 @@ public class CntrlFrmReporteMatenimientoInstalacion extends GenericForwardCompos
 	public void setMaterialActividad(MaterialActividad materialActividad) {
 		this.materialActividad = materialActividad;
 	}
-	
-	
-	
-}
 
+}
